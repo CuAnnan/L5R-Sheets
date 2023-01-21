b16e49fa999d2ef327d6fa55c1d52498258e350d
Wing <eamonn.kearns@so-4pt.net>
2023-01-21 06:48:42 +0000

    Basic data structure in place.

--- sheet.js
+++ sheet.js
@@ -1,17 +1,87 @@
 import xlsx from 'xlsx';
 import fetch from 'node-fetch';
 
+class Rollable
+{
+    constructor(roll, keep)
+    {
+        this.roll = roll;
+        this.keep = keep;
+    }
+}
+
+class Trait extends Rollable
+{
+    constructor(name, value)
+    {
+        super(value, value);
+        this.name = name;
+        this.value = value;
+    }
+}
+
+class Ring extends Trait
+{
+
+}
+
+class Skill extends Rollable
+{
+    constructor(name, trait, value)
+    {
+        super(trait.value + value, value);
+        this.name = name;
+        this.value = value;
+        this.trait = trait.name;
+    }
+}
+
+
 export default class Sheet {
     constructor(json) {
-        this.rings = {
-            air: Math.min(json.traits.reflexes, json.traits.awareness),
-            earth: Math.min(json.traits.stamina, json.traits.willpower),
-            fire: Math.min(json.traits.agility, json.traits.intelligence),
-            water: Math.min(json.traits.strength, json.traits.perception),
-            void: json.void,
-        };
+        this.traits = {};
+        this._rollables = {};
+        for(let [traitName, score] of Object.entries(json.traits))
+        {
+            let trait = new Trait(traitName, score);
+            let lcTraitName = traitName.toLowerCase();
+            this.traits[lcTraitName] = trait;
+            this._rollables[lcTraitName] = trait;
+        }
 
-        this.traits = json.traits;
+        this.rings = {
+            air: new Ring('Air', Math.min(this.traits.reflexes.value, this.traits.awareness.value)),
+            earth: new Ring('Earth', Math.min(this.traits.stamina.value, this.traits.willpower.value)),
+            fire: new Ring('Fire', Math.min(this.traits.agility.value, this.traits.intelligence.value)),
+            water: new Ring( 'Water', Math.min(this.traits.strength.value, this.traits.perception.value)),
+            void: new Ring('Void', json.void),
+        };
+        for(let [ringName, ring] of Object.entries(this.rings))
+        {
+            this._rollables[ringName] = ring;
+        }
+        this.skills = {};
+        for(let [skillName, skillJSON] of Object.entries(json.skills))
+        {
+            let traitName = skillJSON.trait.toLowerCase();
+            let trait;
+            if(Object.keys(this.traits).indexOf(traitName) >= 0)
+            {
+                trait = this.traits[traitName];
+            }
+            else if(Object.keys(this.rings).indexOf(traitName) >= 0)
+            {
+                trait= this.rings[traitName];
+            }
+            let skill = new Skill(skillJSON.name, trait, skillJSON.value);
+            let skillNameLC = skill.name.toLowerCase();
+            if(this.skills[skillNameLC] || this.traits[skillNameLC] || this.rings[skillNameLC])
+            {
+                throw new Error(`Duplicate field key entry ${skill.name}`)
+            }
+            this.skills[skillNameLC] = skill;
+            this._rollables[skillNameLC] = skill;
+        }
     }
 
     toJSON()
@@ -30,20 +100,36 @@ export default class Sheet {
         let baseSheet = document.Sheets[document.SheetNames[0]];
         let spellSheet = document.Sheets[document.SheetNames[1]];
 
+
+        let traitRange = xlsx.utils.decode_range('C2:D13');
         let traits = {};
-        let range = xlsx.utils.decode_range('C2:D13');
-        for (let row = range.s.r; row < range.e.r; row++) {
-            let trait_cell = baseSheet[xlsx.utils.encode_cell({c: range.s.c, r: row})];
-            if (trait_cell) {
-                let value_cell = baseSheet[xlsx.utils.encode_cell({c: range.e.c, r: row})];
-                traits[trait_cell.v.toLowerCase()] = parseInt(value_cell.v);
+        for (let row = traitRange.s.r; row < traitRange.e.r; row++)
+        {
+            let key_cell = baseSheet[xlsx.utils.encode_cell({c: traitRange.s.c, r: row})];
+            if (key_cell) {
+                let value_cell = baseSheet[xlsx.utils.encode_cell({c: traitRange.e.c, r: row})];
+                traits[key_cell.v] = parseInt(value_cell.v);
             }
         }
 
+        let skillRange = xlsx.utils.decode_range('F3:I22');
+        let skills = {};
+        for(let row = skillRange.s.r; row < skillRange.e.r; row++)
+        {
+            let skill_key_cell = baseSheet[xlsx.utils.encode_cell({c:skillRange.s.c, r:row})];
+            if(skill_key_cell)
+            {
+                let trait_key_cell = baseSheet[xlsx.utils.encode_cell({c:skillRange.s.c + 2, r:row})];
+                let value_cell = baseSheet[xlsx.utils.encode_cell({c:skillRange.s.c + 3, r:row})];
+                let skill = {name:skill_key_cell.v, trait:trait_key_cell.v, value:value_cell.v}
+                skills[skill.name] = skill;
+            }
+        }
 
         let sheet = new Sheet({
             void: baseSheet['B15'].v,
-            traits: traits
+            traits: traits,
+            skills:skills
         });
 
         return sheet;
