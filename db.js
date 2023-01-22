0171d1c22ce3bd75cf7aa838f6b6401d1f051e29
Wing <eamonn.kearns@so-4pt.net>
2023-01-22 09:27:55 +0000

    case error

--- db.js
+++ db.js
@@ -1,7 +1,17 @@
 import {MongoClient} from 'mongodb';
+import fs from "fs";
+const jsonFile = fs.readFileSync('./config.json').toString();
+const conf = JSON.parse(jsonFile);
 
-let mongoClient = new MongoClient('mongodb://localhost:27017/l5r');
-let db = null;
+let userStuff = '';
+if(conf.mongo)
+{
+    userStuff = `${conf.mongo.user}:${conf.mongo.password}@`;
+}
+
+let mongoUrl = `mongodb://${userStuff}localhost:27017/l5r`;
+
+let mongoClient = new MongoClient(mongoUrl);
 
 console.log('Checking for mongo instance');
 
